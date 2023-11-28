f6c417b43af54b1563dd14d42656e853ccc76308
DuvanCalderon <79523677+DuvanCalderon@users.noreply.github.com>
2023-11-27 19:32:08 -0500

    Delete AgregarContacto.java

--- AgregarContacto.java
+++
@@ -1,9 +0,0 @@
-package agregarcontacto;
-
-public class AgregarContacto {
-
-    public static void main(String[] args) {
-        Formulario form = new Formulario();
-        form.setVisible(true); 
-    }
-}
