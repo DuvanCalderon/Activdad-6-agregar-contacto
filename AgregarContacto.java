e9170f78d3cacd67c7a27410a38ab8745e5b1b7f
DuvanCalderon <79523677+DuvanCalderon@users.noreply.github.com>
2023-11-27 19:19:50 -0500

    Add files via upload

---
+++ AgregarContacto.java
@@ -0,0 +1,9 @@
+package agregarcontacto;
+
+public class AgregarContacto {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true); 
+    }
+}
